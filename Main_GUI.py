1015d5fba78e93c824a8014ff68f4fb669ed70dd
Jerry <vastepiphany@gmail.com>
2023-11-15 09:23:26 +0800

    Second Commit.

--- Main_GUI.py
+++ Main_GUI.py
@@ -1,33 +1,34 @@
-import tkinter as tk
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *  # 导入常量，例如 LEFT, RIGHT 等
 from open_signup import open_signup_window
 
-#创建窗口
-window = tk.Tk()
-#设置窗口标题
+# 创建窗口，使用 ttkbootstrap 的风格
+window = ttk.Window(themename='flatly')  # 可以选择不同的主题
 window.title("Our Social Club")
-#设置窗口默认大小
 window.geometry("1024x768")
 
+# 创建一个自定义样式
+style = ttk.Style()
+style.configure('Custom.TLabel', font=('Bradley Hand ITC', 18))
+style.configure('Custom.TButton', font=('Helvetica', 12))
 
-#主题标签
-theme_label = tk.Label(window,text="Welcome to Our Social Club!",font=("Bradley Hand ITC",18))
+# 主题标签
+theme_label = ttk.Label(window, text="Welcome to Our Social Club!", style='Custom.TLabel')
 theme_label.pack(pady=20)
 
 # 创建 Sign In 按钮
-sign_in_button = tk.Button(window, text="Sign In", font=("Helvetica", 12))
+sign_in_button = ttk.Button(window, text="Sign In", bootstyle=PRIMARY, style='Custom.TButton')
 sign_in_button.pack(pady=10)
 
 # 创建 Sign Up 部分
-signup_frame = tk.Frame(window)  # 使用 Frame 来组织内容
-signup_label = tk.Label(signup_frame, text="Don't have an account yet?", font=("Helvetica", 12))
-signup_label.pack(side=tk.LEFT, padx=5)  # padx 是水平方向的内边距
-sign_up_button = tk.Button(signup_frame, text="Sign Up", font=("Helvetica", 12),command=lambda:open_signup_window(window))
-#说明：sign_up在被点击后将运行sign_up函数弹出新窗口，不能直接将函数open_signup_window(window)传给command否则将直接运行，正确的是借助lambda 创建了一个匿名函数，它在被调用时执行 open_signup_window(window)
-sign_up_button.pack(side=tk.LEFT)
+signup_frame = ttk.Frame(window)  # 使用 Frame 来组织内容
+signup_label = ttk.Label(signup_frame, text="Don't have an account yet?", font=("Helvetica", 12))
+signup_label.pack(side=LEFT, padx=5)  # padx 是水平方向的内边距
+sign_up_button = ttk.Button(signup_frame, text="Sign Up", bootstyle=SUCCESS, style='Custom.TButton', command=lambda: open_signup_window(window))
+sign_up_button.pack(side=LEFT)
 
 # 将 Sign Up 部分放置到窗口中
 signup_frame.pack(pady=10)
 
-
-#运行GUI主循环
+# 运行 GUI 主循环
 window.mainloop()
--- open_signup.py
+++ open_signup.py
@@ -1,10 +1,15 @@
-import tkinter as tk
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *  # 导入常量
 
 def open_signup_window(window):
-    #新界面基本内容确定
-    signup_window = tk.Toplevel(window)
+    # 创建一个自定义样式
+    style = ttk.Style()
+    style.configure('Custom.TButton', font=('Helvetica', 12))
+
+    # 新界面基本内容确定
+    signup_window = ttk.Toplevel(window)
     signup_window.title("Sign Up")
 
-    tk.Label(signup_window,text="Freshman? Just give us your details to let us know!",font=("Helvetica",14)).pack(pady=10)
-    tk.Entry(signup_window).pack()
-    tk.Button(signup_window,text="Submit",font=("Helvetica",12)).pack(pady=10)
+    ttk.Label(signup_window, text="Freshman? Just give us your details to let us know!", font=("Helvetica", 14)).pack(pady=10)
+    ttk.Entry(signup_window).pack()
+    ttk.Button(signup_window, text="Submit", bootstyle=PRIMARY, style='Custom.TButton').pack(pady=10)
